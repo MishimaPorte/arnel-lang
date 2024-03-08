f412eb11daf274c95641886a5eb3160d4ce807ae
Арсений Байгузин <porte@apsolutions.ru>
2024-03-09 00:12:04 +0300

    main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#define __STRINGS_IMPLEMENTATION
+#define HASHMAP_IMPLEMENTATION
+#include "types.h"
+#include "compiler.h"
+#include "parser.h"
+#include "da.h"
+#include "lexer.h"
+#include <stddef.h>
+#include <stdio.h>
+#include "sv.h"
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    struct compiler_t comp = {0};
+    init_compiler(&comp);
+    comp_set_output(&comp, stdout); //FIXME: compiling to stdout because all the big languages do so
+    comp_file(&comp, "main.ar");
+    return 0;
+}
